8109b2fb26c2c92ee3d9f39da4477cea1faae4ab
Nikolai Darinsky <n.darinsky@gmail.com>
2019-11-13 11:48:10 +0300

    try node express server

---
+++ AwsServerless/server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.get('/', (req, res)=> {
+    res.send("test Server");
+})
+
+app.post('/hello', (req,res)=>{
+    let body = req.body;
+    body.message = "Hello " + body.name;
+
+    res.json(body);
+})
+
+app.listen(3000, ()=>{
+    console.log("Server listening on port 3000");
+})
