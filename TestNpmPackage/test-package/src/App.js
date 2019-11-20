d489d0feb3c906517fbc408c3ea1da7ec586bbdb
Nikolai Darinsky <n.darinsky@gmail.com>
2019-11-20 14:34:37 +0300

    Create my npm package

---
+++ TestNpmPackage/test-package/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import tiny from "@dargie/pmpackage"
+
+function App() {
+  console.log(tiny("aaa ddd fff"))
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
