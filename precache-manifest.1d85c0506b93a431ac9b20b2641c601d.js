d95021fa22ff8e0c7054e50cd1de75425c7d52a5
Nikolai Darinsky <n.darinsky@gmail.com>
2020-01-20 15:09:14 +0300

    Updates

---
+++ precache-manifest.1d85c0506b93a431ac9b20b2641c601d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f139097e188ad4d4e73459a43875cce9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b906f3105d792d10c81d",
+    "url": "/static/css/main.770c58e9.chunk.css"
+  },
+  {
+    "revision": "ef33967d85963ca12d00",
+    "url": "/static/js/2.b0983e84.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b0983e84.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b906f3105d792d10c81d",
+    "url": "/static/js/main.a90a9b14.chunk.js"
+  },
+  {
+    "revision": "e3f6cca2499cc0ccc243",
+    "url": "/static/js/runtime-main.b2109bab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
